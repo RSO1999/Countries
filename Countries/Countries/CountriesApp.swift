047a168b0d4c2329b163926359ab74d1643ef533
Ryan O. <150993053+RSO1999@users.noreply.github.com>
2025-10-15 21:33:16 -0500

    Add files via upload

---
+++ Countries/Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Ryan Ortiz on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
